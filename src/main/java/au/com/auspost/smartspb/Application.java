7880938aa87a2bd0eefd2cc70147e31e798a8d50
David Howe <davidrh@optusnet.com.au>
2016-11-13 06:03:51 +1100

    Added deployment configuration for infra
    MySQL database hosted at AWS
    Updated sample data IMEI for actual 3G board

--- src/main/java/au/com/auspost/smartspb/Application.java
+++ src/main/java/au/com/auspost/smartspb/Application.java
@@ -2,10 +2,16 @@ package au.com.auspost.smartspb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
